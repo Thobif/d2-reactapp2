b1190250dd777e880396122af95e71782303fe49
Thobif <jirayootpandee537@gmail.com>
2021-09-23 18:24:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+
+import React from 'react';
+
 import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom'
+
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import Navbar from  './pages/Navbar';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Router>
+     <div className="container-fluid">  
+     <Navbar />
+      <Home />
+      <About />
+      <Contact />
+      <Profile />
+    
+
+     </div>
+   </Router>
   );
 }
 
